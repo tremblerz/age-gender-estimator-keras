fa46363da92395f93f99d8bace6c1bdc6235a607
Kinar R <kinar.ravishankar@ymedialabs.com>
2018-07-18 12:25:10 +0530

    Removed commented block of code

--- export_android/convert_keras_tf_lite.py
+++ export_android/convert_keras_tf_lite.py
@@ -1,27 +1,3 @@
-# import tensorflow as tf
-# from tensorflow.python.platform import gfile
-#
-# model_filename = 'tensorflow_model/constant_graph_weights.pb'
-# with gfile.FastGFile(model_filename, 'rb') as f:
-#     graph_def = tf.GraphDef()
-#     graph_def.ParseFromString(f.read())
-#     g_in = tf.import_graph_def(graph_def)
-#
-# with tf.Session(graph=g_in) as sess:
-#     # print([n.name for n in tf.get_default_graph().as_graph_def().node])
-#
-#     input_tensors = [sess.graph.get_tensor_by_name('import/the_input:0')]
-#     output_tensors = [sess.graph.get_tensor_by_name('import/output_node0:0'),
-#                       sess.graph.get_tensor_by_name('import/output_node1:0')
-#                       ]
-#
-#     print(input_tensors[0].get_shape())
-#
-#     print(input_tensors, output_tensors)
-#
-#     tflite_model = tf.contrib.lite.toco_convert(sess.graph_def, input_tensors, output_tensors)
-#     open("converted_model.tflite", "wb").write(tflite_model)
-
 import tensorflow as tf
 
 
